b894589f35209f23aadc33425744001db71d7ce9
Lindsey Miller <lindc.miller@gmail.com>
2015-10-26 09:34:46 -0400

    render json in serializer and todo_controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,67 +1,44 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo, only: [:edit, :update, :destroy]
 
   # GET /todos
   # GET /todos.json
   def index
-    @todos = Todo.all
-  end
-
-  # GET /todos/1
-  # GET /todos/1.json
-  def show
-  end
-
-  # GET /todos/new
-  def new
-    @todo = Todo.new
-  end
-
-  # GET /todos/1/edit
-  def edit
+    render json: Todo.all
   end
 
   # POST /todos
   # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    if @todo.save
+      render json: @todo, status: 201
+    else
+      render json: @todo.errors, status: 422
     end
   end
 
   # PATCH/PUT /todos/1
   # PATCH/PUT /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo = Todo.find(params[:id])
+   if @todo.update_attributes(todo_params)
+     render json: @todo, status: :ok
+   else
+     render json: @todo.errors, status: 422
+   end
   end
 
   # DELETE /todos/1
   # DELETE /todos/1.json
   def destroy
+    @todo = Todo.find(params[:id])
     @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: nil, status: :ok
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
